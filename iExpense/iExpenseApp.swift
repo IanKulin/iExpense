c5f8d9c173837dbc4de0748fb2e533b2eb2583bc
Ian Bailey <IanKulin@kulin.com.au>
2022-09-28 06:17:41 +0800

    tutorial version

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Ian Bailey on 27/9/2022.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
